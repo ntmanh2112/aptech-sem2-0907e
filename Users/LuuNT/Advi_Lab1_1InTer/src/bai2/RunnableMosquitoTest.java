0c8fa03302faf17cbd25c5b800079bf93c619b08
LuuTn.hv <Luutn.hv@gmail.com>
2010-05-26 15:38:08 +0000

    Up

---
+++ Users/LuuNT/Advi_Lab1_1InTer/src/bai2/RunnableMosquitoTest.java
@@ -0,0 +1,34 @@
+package bai2;
+
+
+public class RunnableMosquitoTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Mosquito m1 = new Mosquito(12);
+		Mosquito m2 = new Mosquito(21);
+		Mosquito m3 = new Mosquito(22);
+		Mosquito m4 = new Mosquito(32);	
+		
+		Thread t1 = new Thread(m1);
+		t1.start();
+		
+		Thread t2 = new Thread(m2);
+		t2.start();
+		
+		Thread t3 = new Thread(m3);
+		t3.start();
+		
+		Thread t4 = new Thread(m4);
+		t4.start();
+		
+		System.out.println(t1.getName());
+		System.out.println(t2.getName());
+		System.out.println("No of Thread :"+ Thread.activeCount());
+	}
+
+}
