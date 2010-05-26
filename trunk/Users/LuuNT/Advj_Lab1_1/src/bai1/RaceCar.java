5f5d1f0fc109584557f7dd8daadefd7232936ad9
Luutn.hv@gmail.com <Luutn.hv@gmail.com@bee8b695-3e92-5402-091d-c5ee85b75c4a>
2010-05-26 15:38:08 +0000

    Up

---
+++ trunk/Users/LuuNT/Advj_Lab1_1/src/bai1/RaceCar.java
@@ -0,0 +1,25 @@
+package bai1;
+
+public class RaceCar extends Thread {
+
+	int finish;
+	String name;
+	
+	public RaceCar(int finish , String name){
+		this.finish = finish;
+		this.name= name;
+	}
+	
+	public void run(){
+		for(int i=0 ; i<finish ; i++){
+			System.out.println(name+": "+(i+1));
+			try{
+				Thread.sleep(Math.round(Math.random()*5000));
+				
+			}catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+		System.out.println(name+ " finished");
+	}
+}
