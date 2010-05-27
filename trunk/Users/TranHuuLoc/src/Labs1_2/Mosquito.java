7d21818b5b928b7902e7031e2a91a2bcab3e6e62
locth.hv@gmail.com <locth.hv@gmail.com@bee8b695-3e92-5402-091d-c5ee85b75c4a>
2010-05-27 11:18:40 +0000

    add code

---
+++ trunk/Users/TranHuuLoc/src/Labs1_2/Mosquito.java
@@ -0,0 +1,24 @@
+package Labs1_2;
+
+public class Mosquito implements Runnable{
+	private int id;
+	public Mosquito(int id)
+	{
+	this.id = id;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		if (id == 12) {
+		try {
+			Thread.sleep(10000);
+		} catch(Exception e){}
+		}
+		for (int i=0; i<3; i++) {
+			System.out.println("Vo ve " + id);
+		}
+
+	}
+
+}
